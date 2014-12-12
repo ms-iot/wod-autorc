5e36413cd1a2a4cbf9d4b3a2697555f2651b2b8d
Kyle Olive <t-kyol@microsoft.com>
2014-12-11 16:17:06 -0800

    copyright

--- WoD-AutoRCCar/PID.cpp
+++ WoD-AutoRCCar/PID.cpp
@@ -1,3 +1,8 @@
+// Copyright(c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the BSD 2 - Clause License.
+// See License.txt in the project root for license information.
+
+
 #include "PID.h"
 #include "arduino.h"
 #include <vector>
